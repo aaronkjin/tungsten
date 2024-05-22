d5c158772e67fc1cae30462003dc204616e78df8
Aaron Jin <aaronkjin@gmail.com>
2024-05-22 14:56:23 -0700

    pemdas

--- crust/src/ast/lexer.rs
+++ crust/src/ast/lexer.rs
@@ -1,1 +1,30 @@
-pub mod lexer;
+pub enum TokenKind {
+    Number(i64),
+    // PEMDAS operators
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    LeftParen,
+    RightParen,
+}
+
+pub struct TextSpan {
+    start: usize,
+    end: usize,
+}
+
+impl TextSpan {
+    
+    pub fn new(start: usize, end: usize) -> Self {
+        TextSpan { start, end }
+    }
+
+    pub fn length(&self) -> usize {
+        self.end - self.start
+    }
+}
+
+pub struct Token {
+    kind: TokenKind,
+}
