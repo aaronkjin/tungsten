65b08631f4bc83046daf0f258fd09cc397e2cea3
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-05 23:47:44 -0700

    output

--- crust/target/debug/deps/crust-2eefb55cf70c910b.d
+++ crust/target/debug/deps/crust-2eefb55cf70c910b.d
@@ -1,6 +1,7 @@
-/Users/aaronjin/Downloads/Projects/tungsten/crust/target/debug/deps/crust-2eefb55cf70c910b: src/main.rs src/ast/mod.rs
+/Users/aaronjin/Downloads/Projects/tungsten/crust/target/debug/deps/crust-2eefb55cf70c910b: src/main.rs src/ast/mod.rs src/ast/lexer.rs
 
-/Users/aaronjin/Downloads/Projects/tungsten/crust/target/debug/deps/crust-2eefb55cf70c910b.d: src/main.rs src/ast/mod.rs
+/Users/aaronjin/Downloads/Projects/tungsten/crust/target/debug/deps/crust-2eefb55cf70c910b.d: src/main.rs src/ast/mod.rs src/ast/lexer.rs
 
 src/main.rs:
 src/ast/mod.rs:
+src/ast/lexer.rs:
