c44bab64b9c9a827e45d8d19dcbed60103d4ef9c
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-24 19:07:47 -0700

    init ast for parser

--- crust/src/ast/parser.rs
+++ crust/src/ast/parser.rs
@@ -1,2 +1,11 @@
 pub mod lexer;
 pub mod parser;
+
+pub struct Ast {
+    pub statements: Vec<ASTStatement>,
+}
+
+// Useful for Rust's pattern-matching
+pub enum ASTStatementKind {}
+
+pub struct ASTStatement {}
