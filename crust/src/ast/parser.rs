886126f2f64b079aa83dffe6099bedb8545c5da7
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-25 19:32:32 -0700

    init from_input

--- crust/src/ast/parser.rs
+++ crust/src/ast/parser.rs
@@ -7,4 +7,15 @@ impl Parser {
     pub fn new() -> Self {
         Self { tokens: Vec::new(), current: 0 }
     }
+
+    pub fn from_input(input: &str) -> Self {
+        let mut lexer = super::lexer::Lexer::new(input);
+        let mut tokens = Vec::new();
+
+        while let Some(token) = lexer.next_token() {
+            tokens.push(token);
+        }
+
+        Self { tokens, current: 0 }
+    }
 }
