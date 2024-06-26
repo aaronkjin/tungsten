cd6d9f3be1b9d6847cb7c8955f4f842ef506e803
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-25 19:30:58 -0700

    init new

--- crust/src/ast/parser.rs
+++ crust/src/ast/parser.rs
@@ -2,3 +2,9 @@ pub struct Parser {
     tokens: Vec<super::lexer::Token>,
     current: usize, // Pointer to cur token
 }
+
+impl Parser {
+    pub fn new() -> Self {
+        Self { tokens: Vec::new(), current: 0 }
+    }
+}
