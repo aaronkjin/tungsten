cbdfe78e43297b4eaea23671eb02bf8e751c9d1a
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-06-25 19:28:29 -0700

    oops meant for mod

--- crust/src/ast/mod.rs
+++ crust/src/ast/mod.rs
@@ -1,1 +1,23 @@
 pub mod lexer;
+pub mod parser;
+
+pub struct Ast {
+    pub statements: Vec<ASTStatement>,
+}
+
+// Useful for Rust's pattern-matching
+pub enum ASTStatementKind {
+    Expression(ASTExpression),
+}
+
+pub struct ASTStatement {
+    kind: ASTStatementKind,
+}
+
+pub enum ASTExpressionKind {
+    Number(i64),
+}
+
+pub struct ASTExpression {
+    kind: ASTExpressionKind,
+}
