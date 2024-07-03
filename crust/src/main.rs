152dd900c92c141e98ae5d2d2990751e801afc52
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-07-02 23:33:22 -0700

    remove specifiers

--- crust/src/main.rs
+++ crust/src/main.rs
@@ -3,7 +3,7 @@ mod ast;
 fn main() {
     let input = "7 + 3 * (10 / (12 / (3 + 1) - 1))";
 
-    let mut lexer = ast::lexer::Lexer::new(input);
+    let mut lexer = Lexer::new(input);
     let mut tokens = Vec::new();
 
     while let Some(token) = lexer.next_token() {
