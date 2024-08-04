52de463a288f6ad18708e8a8c6ab0de68dd484fd
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-03 22:44:34 -0700

    visit number

--- crust/src/ast/evaluator.rs
+++ crust/src/ast/evaluator.rs
@@ -1,3 +1,11 @@
+use crate::ast::ASTVisitor;
+
 pub struct ASTEvaluator {
     pub last_value: Option<i64>,
+}
+
+impl ASTVisitor for ASTEvaluator {
+    fn visit_number(&mut self, number: &ASTNumberExpression) {
+        self.last_value = Some(number.value);
+    }
 }
