cf7c62368e662636a906701dad297070a67e7236
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-03 22:46:18 -0700

    visit binary expr

--- crust/src/ast/evaluator.rs
+++ crust/src/ast/evaluator.rs
@@ -8,4 +8,17 @@ impl ASTVisitor for ASTEvaluator {
     fn visit_number(&mut self, number: &ASTNumberExpression) {
         self.last_value = Some(number.value);
     }
+
+    fn visit_binary_expression(&mut self, expr: &ASTBinaryExpression) {
+        expr.left.visit(self);
+        let left = self.last_value.unwrap();
+
+        expr.right.visit(self);
+        let right = self.last_value.unwrap();
+
+        self.last_value = Some(match expr.operator.kind {
+            ASTBinaryOperatorKind::Plus => left + right,
+            ASTBinaryOperatorKind::Minus => left - right,
+        });
+    }
 }
