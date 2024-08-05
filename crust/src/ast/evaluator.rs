73e754b5989991ae573e48ba9bb516af31926762
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-04 21:20:08 -0700

    evaluator constructor

--- crust/src/ast/evaluator.rs
+++ crust/src/ast/evaluator.rs
@@ -4,6 +4,12 @@ pub struct ASTEvaluator {
     pub last_value: Option<i64>,
 }
 
+impl ASTEvaluator {
+    pub fn new() -> Self {
+        Self { last_value: None }
+    }
+}
+
 impl ASTVisitor for ASTEvaluator {
     fn visit_number(&mut self, number: &ASTNumberExpression) {
         self.last_value = Some(number.value);
