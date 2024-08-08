441b1e5c2c0e032a141a71edcd032898383d37ad
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-07 21:19:27 -0700

    diagnostics bag

--- crust/src/main.rs
+++ crust/src/main.rs
@@ -19,6 +19,8 @@ fn main() {
 
     println!("{:?}", tokens);
 
+    let diagnostics_bag = Rc::new(RefCell::new(diagnostics::DiagnosticsBag::new()));
+
     // Part II: Parser
     let mut ast: Ast = Ast::new();
     let mut parser = Parser::new(tokens);
