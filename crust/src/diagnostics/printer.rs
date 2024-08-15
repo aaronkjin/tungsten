d4567330247570741bbefa93d82f5881a73faa79
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-14 20:41:08 -0700

    diagnostics printer constructor

--- crust/src/diagnostics/printer.rs
+++ crust/src/diagnostics/printer.rs
@@ -4,3 +4,12 @@ pub struct DiagnosticsPrinter<'a> {
     text: &'a SourceText,
     diagnostics: &'a [Diagnostic],
 }
+
+impl<'a> DiagnosticsPrinter<'a> {
+    pub fn new(text: &'a SourceText, diagnostics: &'a [Diagnostic]) -> Self {
+        Self {
+            text,
+            diagnostics,
+        }
+    }
+}
