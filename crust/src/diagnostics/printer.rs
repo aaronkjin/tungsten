c3a5d53ac4474e5f5276cd10bed363040c437ecc
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-08-14 21:27:00 -0700

    how error msg looks

--- crust/src/diagnostics/printer.rs
+++ crust/src/diagnostics/printer.rs
@@ -16,6 +16,9 @@ impl<'a> DiagnosticsPrinter<'a> {
     /*
      * Stringify the diagnostic by using the format:
      * let <red>x<reset> = 5;
+     *          ^
+     *          |
+     *          +-- This is the error message (<line>:<column>)
      */
     pub fn stringify_diagnostic(&self, diagnostic: &Diagnostic) -> Result<String, ()> {}
 }
