4b29f38beb619c46cc76173132cab62c83dbb6fb
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-09-12 22:19:19 -0700

    example of let statement

--- crust/src/ast/parser.rs
+++ crust/src/ast/parser.rs
@@ -71,11 +71,11 @@ impl Parser {
     }
 
     fn parse_let_statement(&mut self) -> ASTStatement {
-        self.consume_and_check(TokenKind::Let);
-        let identifier = self.consume_and_check(TokenKind::Identifier);
-        self.consume_and_check(TokenKind::Equals);
+        self.consume_and_check(TokenKind::Let); // let
+        let identifier = self.consume_and_check(TokenKind::Identifier); // x
+        self.consume_and_check(TokenKind::Equals); // =
+        let expr = self.parse_expression(); // 21
 
-        let expr = self.parse_expression();
         return ASTStatement::let_statement(identifier.clone(), expr);
     }
 
