612779becda9ce9eb34a71291cfefab0fae182ac
aaronkjin <aaronkjin@gmail.com>
2024-10-14 16:09:53 -0700

    clean up for evaluator

--- crust/src/ast/evaluator.rs
+++ crust/src/ast/evaluator.rs
@@ -1,45 +1,28 @@
+use std::collections::HashMap;
 use crate::ast::{
     ASTBinaryExpression,
     ASTBinaryOperatorKind,
     ASTLetStatement,
     ASTNumberExpression,
-    ASTVisitor,
-    TextSpan,
     ASTParenthesizedExpression,
     ASTVariableExpression,
+    ASTVisitor,
 };
-
-use crate::DiagnosticsBagCell;
-
-use std::collections::HashMap;
+use crate::ast::lexer::TextSpan;
 
 pub struct ASTEvaluator {
     pub last_value: Option<i64>,
     pub variables: HashMap<String, i64>,
-    pub diagnostics: DiagnosticsBagCell,
 }
 
 impl ASTEvaluator {
     pub fn new() -> Self {
-        Self { last_value: None, variables: HashMap::new(), diagnostics }
+        Self { last_value: None, variables: HashMap::new() }
     }
 }
 
 impl ASTVisitor for ASTEvaluator {
     fn visit_let_statement(&mut self, let_statement: &ASTLetStatement) {
-        /*
-        self.visit_expression(&let_statement.initializer);
-
-        if let Some(value) = self.last_value {
-            self.variables.insert(let_statement.identifier.span.literal.clone(), value);
-        } else {
-            println!(
-                "Error: Could not evaluate let statement for variable {}",
-                let_statement.identifier.span.literal
-            );
-        }
-        */
-
         self.visit_expression(&let_statement.initializer);
         self.variables.insert(
             let_statement.identifier.span.literal.clone(),
@@ -47,44 +30,25 @@ impl ASTVisitor for ASTEvaluator {
         );
     }
 
+    fn visit_variable_expression(&mut self, variable_expression: &ASTVariableExpression) {
+        self.last_value = Some(
+            *self.variables.get(&variable_expression.identifier.span.literal).unwrap()
+        );
+    }
+
     fn visit_number_expression(&mut self, number: &ASTNumberExpression) {
         self.last_value = Some(number.number);
     }
 
-    fn visit_error(&mut self, _span: &TextSpan) {
-        println!("Error: Invalid expression or token found.");
-        self.last_value = None;
+    fn visit_error(&mut self, span: &TextSpan) {
+        todo!()
     }
 
     fn visit_binary_expression(&mut self, expr: &ASTBinaryExpression) {
-        /*
-        self.visit_expression(&expr.left);
-        let left = match self.last_value {
-            Some(value) => value,
-            None => {
-                println!("Error: Could not evaluate left side of binary expression.");
-                return;
-            }
-        };
-        */
-
         self.visit_expression(&expr.left);
         let left = self.last_value.unwrap();
-
-        /*
-        self.visit_expression(&expr.right);
-        let right = match self.last_value {
-            Some(value) => value,
-            None => {
-                println!("Error: Could not evaluate right side of binary expression.");
-                return;
-            }
-        };
-        */
-
         self.visit_expression(&expr.right);
         let right = self.last_value.unwrap();
-
         self.last_value = Some(match expr.operator.kind {
             ASTBinaryOperatorKind::Plus => left + right,
             ASTBinaryOperatorKind::Minus => left - right,
@@ -98,17 +62,5 @@ impl ASTVisitor for ASTEvaluator {
         parenthesized_expression: &ASTParenthesizedExpression
     ) {
         self.visit_expression(&parenthesized_expression.expression);
-    }
-
-    fn visit_variable_expression(&mut self, variable_expression: &ASTVariableExpression) {
-        let var_name = &variable_expression.identifier.span.literal;
-
-        if let Some(value) = self.variables.get(var_name) {
-            self.last_value = Some(*value);
-        } else {
-            let mut diagnostics = self.diagnostics.borrow_mut();
-            diagnostics.report_undeclared_variable(&variable_expression.identifier);
-            self.last_value = None;
-        }
     }
 }
