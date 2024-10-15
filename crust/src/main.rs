834868fbadf44ebf5156903509c50384045ceed7
aaronkjin <aaronkjin@gmail.com>
2024-10-14 17:48:23 -0700

    making main more readable

--- crust/src/main.rs
+++ crust/src/main.rs
@@ -39,6 +39,7 @@ impl SymbolChecker {
 impl ASTVisitor for SymbolChecker {
     fn visit_let_statement(&mut self, let_statement: &ASTLetStatement) {
         let identifier = let_statement.identifier.span.literal.clone();
+
         self.visit_expression(&let_statement.initializer);
         self.symbols.insert(identifier, ());
     }
@@ -46,6 +47,7 @@ impl ASTVisitor for SymbolChecker {
     fn visit_variable_expression(&mut self, variable_expression: &ASTVariableExpression) {
         if self.symbols.get(&variable_expression.identifier.span.literal).is_none() {
             let mut diagnostics_binding = self.diagnostics.borrow_mut();
+
             diagnostics_binding.report_undeclared_variable(&variable_expression.identifier);
         }
     }
@@ -55,46 +57,62 @@ impl ASTVisitor for SymbolChecker {
     fn visit_error(&mut self, span: &TextSpan) {}
 }
 
-// Main method
 fn main() -> Result<(), ()> {
     let input =
         "
-        let a =10 + 30
+        let a = 10 + 30
         let b = 20
         let d = 10 + e
         let c = (a + b) * d
     ";
+
     let text = text::SourceText::new(input.to_string());
+
+    // Part I: Lexer
     let mut lexer = Lexer::new(input);
     let mut tokens = Vec::new();
+
     while let Some(token) = lexer.next_token() {
         tokens.push(token);
     }
+
     let diagnostics_bag: DiagnosticsBagCell = Rc::new(
         RefCell::new(diagnostics::DiagnosticsBag::new())
     );
+
+    // Part II: Parser
     let mut ast: Ast = Ast::new();
     let mut parser = Parser::new(tokens, Rc::clone(&diagnostics_bag));
+
     while let Some(stmt) = parser.next_statement() {
         ast.add_statement(stmt);
     }
+
+    // Visualizer
     ast.visualize();
+
+    // Diagnostics printer
     check_diagnostics(&text, &diagnostics_bag)?;
     let mut symbol_checker = SymbolChecker::new(Rc::clone(&diagnostics_bag));
     ast.visit(&mut symbol_checker);
     check_diagnostics(&text, &diagnostics_bag)?;
+
+    // Evaluator
     let mut eval = ASTEvaluator::new();
     ast.visit(&mut eval);
     println!("Result: {:?}", eval.last_value);
+
     Ok(())
 }
 
 fn check_diagnostics(text: &SourceText, diagnostics_bag: &DiagnosticsBagCell) -> Result<(), ()> {
     let diagnostics_binding = diagnostics_bag.borrow();
+
     if diagnostics_binding.diagnostics.len() > 0 {
         let diagnostics_printer = DiagnosticsPrinter::new(&text, &diagnostics_binding.diagnostics);
         diagnostics_printer.print();
         return Err(());
     }
+
     Ok(())
 }
