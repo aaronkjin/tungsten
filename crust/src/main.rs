2f94380399497ac8b01e9f1739c92247fdeb1930
aaronkjin <aaronkjin@gmail.com>
2024-11-03 19:21:41 -0800

    clean up main

--- crust/src/main.rs
+++ crust/src/main.rs
@@ -1,47 +1,11 @@
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::hash::Hash;
-use std::rc::Rc;
-use crate::ast::{
-    Ast,
-    ASTBinaryExpression,
-    ASTLetStatement,
-    ASTNumberExpression,
-    ASTParenthesizedExpression,
-    ASTVariableExpression,
-    ASTVisitor,
-};
-use crate::ast::evaluator::ASTEvaluator;
-use crate::ast::lexer::{ Lexer, TextSpan };
-use crate::ast::parser::Parser;
 use crate::compilation_unit::CompilationUnit;
-use crate::diagnostics::DiagnosticsBagCell;
-use crate::diagnostics::printer::DiagnosticsPrinter;
-use crate::text::SourceText;
 
 mod ast;
 mod diagnostics;
 mod text;
 mod compilation_unit;
 
-fn main() -> Result<(), ()> {
-    // AST:
-    /*
-     * Variable decl
-     *  Identifier: a
-     *  Initializer: BinaryExpression
-     *      Left: NumberExpression
-     *          Number: 10
-     *      Operator: Plus
-     *      Right: NumberExpression
-     *          Number: 30
-     */
-
-    // Flattened:
-    /*
-     * Decl, Binary Expr, Number, Number
-     */
-
+fn main() {
     let input =
         "\
         let a = 10 + 30
@@ -52,6 +16,4 @@ fn main() -> Result<(), ()> {
 
     let compilation_unit = CompilationUnit::compile(input);
     compilation_unit.run();
-
-    let Ok(());
 }
