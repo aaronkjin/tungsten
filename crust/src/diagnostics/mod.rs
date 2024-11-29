3b02cc865ccbc18876c962b38e4aa5c7bf26af33
aaronkjin <aaronkjin@gmail.com>
2024-11-28 22:35:49 -0800

    let's do the same for diagnostics mod

--- crust/src/diagnostics/mod.rs
+++ crust/src/diagnostics/mod.rs
@@ -62,3 +62,11 @@ impl DiagnosticsBag {
         );
     }
 }
+
+#[cfg(test)]
+mod test {
+    struct DiagnosticsVerifier {
+        actual: Vec<Diagnostic>,
+        expected: Vec<Diagnostic>,
+    }
+}
