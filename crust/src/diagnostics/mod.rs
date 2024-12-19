c42306eee147c331350dd2a5f7bd570987c3f7e2
aaronkjin <aaronkjin@gmail.com>
2024-12-18 17:36:29 -0800

    use literal

--- crust/src/diagnostics/mod.rs
+++ crust/src/diagnostics/mod.rs
@@ -107,9 +107,10 @@ mod test {
                     '»' => {
                         let start_index = start_index_stack.pop().unwrap();
                         let end_index = index;
-                        let span = TextSpan::new(start_index, end_index);
 
-                        let message = messages[current_position].to_string();
+                        let literal = &raw_text[current_position..end_index];
+                        let span = TextSpan::new(start_index, end_index, literal.to_string());
+                        let message = messages[diagnostics.len()].to_string();
                         let diagnostic = Diagnostic::new(message, DiagnosticKind::Error);
                         diagnostics.push(diagnostic);
                     }
